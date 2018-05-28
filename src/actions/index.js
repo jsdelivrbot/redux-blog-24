04da6ee471664c0fabaed8236059bd1ec8929ded
Jonathan IBOR <jonathan.ibor@hetic.net>
2018-05-28 22:55:06 +0200

    Search, with auto-complete and some other update :tada: :fire:

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,37 +2,68 @@ import {
   FETCH_POSTS,
   ERROR_FETCH_POSTS,
   ACTIVE_POST,
-  DELETE_FETCH_POSTS
+  DELETE_FETCH_POSTS,
+  CREATE_POST,
+  SEARCH_POST
 } from "./actions-type";
 import axios from "axios";
 
 export function fetch_posts() {
   return dispatch => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts?_limit=10")
-      .then(res => {
-        dispatch({ type: FETCH_POSTS, payload: res.data });
-      });
+    axios.get("http://localhost:3000/posts").then(res => {
+      dispatch({ type: FETCH_POSTS, payload: res.data });
+    });
   };
 }
 
 export function delete_fetch_posts(id) {
   return dispatch => {
-    axios(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    axios(`http://localhost:3000/posts/${id}`, {
       method: "DELETE"
     }).then(res => {
-      dispatch({ type: DELETE_FETCH_POSTS, payload: {
-        id,
-        loading: true
-      } });
+      dispatch({
+        type: DELETE_FETCH_POSTS,
+        payload: { id }
+      });
     });
   };
 }
 
 export function active_post(id) {
   return dispatch => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => {
+    axios.get(`http://localhost:3000/posts/${id}`).then(res => {
       dispatch({ type: ACTIVE_POST, payload: res.data });
+    });
+  };
+}
+
+export function create_post(post) {
+  return dispatch => {
+    axios
+      .post("http://localhost:3000/posts/", {
+        ...post
+      })
+      .then(res => dispatch({ type: CREATE_POST, payload: res.data }));
+  };
+}
+
+export function autoComplete(value) {
+  return dispatch => {
+    axios.get("http://localhost:3000/posts/").then(res => {
+      let result;
+      let string = "";
+      value = value.toLowerCase();
+      for (let i = 0; i < value.length; i++) {
+        string = string + value[i];
+      }
+      result = res.data.filter(post => {
+        const lowerCase = post.title.toLowerCase();
+        if (!lowerCase.includes(string)) {
+          return false;
+        }
+        return true;
+      });
+      return dispatch({ type: SEARCH_POST, payload: result });
     });
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 import PostsReducer from "./Posts-reducer";
 import ActivePost from "./activePost-reducer";
+import SearchPost from "./SearchPost-reducer";
+import { reducer as FormReducer } from "redux-form";
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  activePost: ActivePost
+  activePost: ActivePost,
+  form: FormReducer,
+  searchPost: SearchPost
 });
 
 export default rootReducer;
